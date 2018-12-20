ebf12f69babbffeae8ebfb9bfe6bdca2faa52326
Danielle <dani.code.camp@gmail.com>
2018-12-20 17:54:42 -0500

    Updates

---
+++ precache-manifest.261fd852abc4bc67bfd055092d24d271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "83f8fec4a7a670f133fa",
+    "url": "/contactmanager/static/js/main.83f8fec4.chunk.js"
+  },
+  {
+    "revision": "865e04d812dc43a9d354",
+    "url": "/contactmanager/static/js/1.865e04d8.chunk.js"
+  },
+  {
+    "revision": "83f8fec4a7a670f133fa",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "865e04d812dc43a9d354",
+    "url": "/contactmanager/static/css/1.3d687446.chunk.css"
+  },
+  {
+    "revision": "80ebc66ca465397061fbca03b66dad1b",
+    "url": "/contactmanager/index.html"
+  }
+];
